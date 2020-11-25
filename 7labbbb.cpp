e502883a67906fbbf45aefc9034d096e1ed2272a
Eugenito <72038188+Eugenito@users.noreply.github.com>
2020-11-25 12:22:02 +0300

    Add files via upload

---
+++ 7labbbb.cpp
@@ -0,0 +1,45 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	system("chcp 1251");
+	system("cls");
+	int n;
+	char* str;
+	printf("Введите размер строки\n");
+	scanf_s("%d", &n);
+	puts("Enter str:");
+	str = (char*)malloc(n * sizeof(char));
+	rewind(stdin);//Очищает входной поток,  в потоке осталось нажатие Enter и fgets по этому символу заканчивается ,ревайнд уберет enter .
+	fgets(str, n, stdin);
+	for (int i = 0; str[i] != '\n';)
+	{
+
+
+		if (str[i] == ' ')
+		{
+			for (int j = i; str[j] != '\n'; j++)
+			{
+
+				str[j] = str[j + 1];
+
+
+
+			}
+		}
+		else
+		{
+			i++;
+			if (str[i] == ' ') i++;
+		}
+
+	}
+
+	printf("%s", str);
+
+	free(str);
+	return 0;
+}
+
+
